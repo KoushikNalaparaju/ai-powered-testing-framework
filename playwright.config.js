90884b893d544b813c002718aee652ceb20bc518
Koushik Nalaparaju <koushiknalaparaju@mac.mynetworksettings.com>
2026-04-22 23:05:22 -0400

    initial project setup with Playwright

---
+++ playwright.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html', { outputFolder: 'reports/html-report', open: 'never' }],
+    ['list']
+  ],
+  use: {
+    baseURL: 'https://www.saucedemo.com',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
